77b21c0feccc03c5041351b091a0c15eaf7ed380
LakshayBhandari <LakshayBhandari867@gmail.com>
2024-03-19 17:40:52 +0530

    first commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Authform from "./components/Authform";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+         <Image alt="Logo" height="48" width="48" src="/images/logo.jpg"/>
+         <h2>Sign in to your account</h2>
+        </div>
+        <Authform/>
+    </div>
+  );
+}
