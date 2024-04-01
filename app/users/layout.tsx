3a1128759eaa62797261fb3dee827335f232648f
LakshayBhandari <LakshayBhandari867@gmail.com>
2024-04-01 23:34:35 +0530

    sidebar navigation and layout added

---
+++ app/users/layout.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/sidebar/Sidebar";
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className=" h-full">
+    <Sidebar>
+    {children}
+    </Sidebar>
+    </div>;
+}
